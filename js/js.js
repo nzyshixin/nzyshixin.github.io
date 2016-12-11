dfa9ac83c0ca161429642a3b6c6fadc8a59cfba7
nzyxl <1801507818@qq.com>
2016-12-11 19:14:58 +0800

    Signed-off-by: nzyxl <1801507818@qq.com>

---
+++ js/js.js
@@ -0,0 +1,12 @@
+$(function(){
+//	var bs=0;
+//	setInterval(function(){
+//		bs+=1;
+//		if(bs==1){
+//			$('#logo-bs').css({color:'#868585',border:'2px solid #868585',transition:'2s all'});
+//		}else if(bs==2){
+//			$('#logo-bs').css({color:'#fff',border:'2px solid #fff',transition:'2s all'});
+//			bs=0;
+//		}
+//	},1500)
+})
